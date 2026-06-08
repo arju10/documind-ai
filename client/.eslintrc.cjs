e642c2890ed7d3c9f2cf3bf926d8c4fb8bf92932
arju10 <arjuarju71@gmail.com>
2026-06-08 20:58:47 +0600

    chore: add Prettier config at root level &&&& chore(client): add ESLint + Prettier config with TypeScript and React rules

---
+++ client/.eslintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react-refresh/recommended',
+    'plugin:prettier/recommended', // always last
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true },
+  },
+  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'prettier'],
+  rules: {
+    'react/react-in-jsx-scope': 'off', // not needed in React 18
+    'react/prop-types': 'off', // we use TypeScript
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'prettier/prettier': 'error',
+  },
+  settings: {
+    react: { version: 'detect' },
+  },
+};
